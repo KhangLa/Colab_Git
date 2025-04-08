bc9da3bc8bb40f13cc2a89af0f2d740a88fc9f0d
HaGiaLoc <habengialoc@gmal.com>
2025-04-08 09:55:31 +0700

    Add Spike-image.jpg, update script.js and collab_git.html

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const danh_sach_thanh_vien = ["Lộc", "Hào", "Khang"];
+// const danh_sach_thanh_vien = ["Lộc", "Hào", "Khang"];
 
-document.getElementById("actionButton").addEventListener("click", function()
-{
-    const chuoi_ten = danh_sach_thanh_vien.map(ten=> "  - " + ten).join("\n");
-    alert("Các thành viên trong nhóm gồm:\n" + chuoi_ten);
-}
+// document.getElementById("actionButton").addEventListener("click", function()
+// {
+//     const chuoi_ten = danh_sach_thanh_vien.map(ten=> "  - " + ten).join("\n");
+//     alert("Các thành viên trong nhóm gồm:\n" + chuoi_ten);
+// }
+// );
+
+
+document.addEventListener("DOMContentLoaded", function actionButton()
+    {
+        document.getElementById("hinh_anh_Tom").innerHTML = `
+            <img src="Tom-image.jpg" alt = "Hình ảnh của Tom" width = "300"">
+        `
+        document.getElementById("hinh_anh_Tom").style.display = 'block';
+
+        document.getElementById("hinh_anh_Jerry").innerHTML = `
+            <img src="Jerry-image.jpg" alt = "Hình ảnh của Tom" width = "300"">
+        `
+        document.getElementById("hinh_anh_Jerry").style.display = 'block';
+
+        document.getElementById("hinh_anh_Spike").innerHTML = `
+            <img src="Spike-image.jpg" alt = "Hình ảnh của Tom" width = "300"">
+        `
+        document.getElementById("hinh_anh_Spike").style.display = 'block';
+    }
 );
